d9cc00ce4437a5ed783d7d7d8cb6c20e4c560939
Daniel009891 <>
2024-01-02 22:51:08 +0000

    Create first stateful class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
         <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Dan" age="32" />
+        <StatefulGreeting greeting="I'm a stateful class component!" />
     </div>
   );
 }
