b62cfee6ae1ce64d5c87bb7053bc38ba654184fb
Daniel009891 <>
2024-01-10 21:16:36 +0000

    complete component lifecycles material

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-import LifeCyclesCDM from './components/LifeCyclesCDM';
+// import LifeCyclesCDM from './components/LifeCyclesCDM';
+// import LifeCyclesCDU from './components/LifeCyclesCDU';
+import LifeCyclesCWU from './components/LifeCyclesCWU';
 
 
 function App() {
   return (
     <div className="App">
-      <LifeCyclesCDM /> 
+       < LifeCyclesCWU />
     </div>
   );
 }
